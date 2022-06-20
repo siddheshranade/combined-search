67d12cc0b62744c65d31f855012b3a01aecb10ed
Siddhesh Ranade <ranade.siddhesh@gmail.com>
2022-06-20 19:35:59 -0400

    Add hardcoded JS logic for Google, Reddit, Wiki

--- scripts.js
+++ scripts.js
@@ -1,10 +1,62 @@
-let searchButton = document.querySelector('#search');
-searchButton.addEventListener('click', search);
+/* I. Query relevant DOM elements */
 
-function search() {
-    let input = document.querySelector('#input');
-    let url = `https://www.google.com/search?q=${input.value}`;
-    console.log(url);
-    console.log('OPENING');
+const searchForm = document.querySelector('form');
+const textInput = document.querySelector('#search-input');
+const checkboxes = document.querySelectorAll('.checkboxes');
+
+/* II. Add listeners */
+
+searchForm.addEventListener('submit', search);
+
+/* III. Define functions */
+
+function search(e) {
+    e.preventDefault();
+    const searchQuery = getSearchQuery(textInput.value);
+
+    if (document.querySelector('#reddit-check').checked) {
+        searchReddit(searchQuery);
+    }
+    if (document.querySelector('#google-check').checked) {
+        searchGoogle(searchQuery);
+    }
+    if (document.querySelector('#wikipedia-check').checked) {
+        searchWikipedia(searchQuery);
+    }
+}
+
+function searchGoogle(query) {
+    let url = `https://www.google.com/search?q=${query}`;
+    console.log('Opening Google');
+    // window.open(url, '_blank').focus();
     window.open(url);
+    // openInNewTab(url);
+}
+
+function searchWikipedia(query) {
+    let url = `https://en.wikipedia.org/w/index.php?search=${query}`;
+    console.log('Opening Wikipedia');
+    // window.open(url, '_blank').focus();
+    window.open(url);
+    // openInNewTab(url);
+}
+
+function searchReddit(query) {
+    let url = `https://www.reddit.com/search?q=${query}`;
+    console.log('Opening Reddit');
+    // window.open(url, '_blank').focus();
+    window.open(url);
+    // openInNewTab(url);
+}
+
+function openInNewTab(url) {
+    Object.assign(document.createElement('a'), {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        href: url
+    }).click();
+}
+
+function getSearchQuery(str) {
+    return str.replaceAll(' ', '+');
 }
